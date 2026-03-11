64e19ca73e89fb68aa4b880573ab932a5cc1caad
ferpdf <42195393+ferpdf@users.noreply.github.com>
2026-03-10 22:48:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Biosilver Web funcionando");
+window.addEventListener("scroll", function(){
+
+let elements = document.querySelectorAll(".card, .sector, .galeria img");
+
+elements.forEach(function(el){
+
+let position = el.getBoundingClientRect().top;
+
+let screen = window.innerHeight;
+
+if(position < screen - 100){
+
+el.style.opacity = "1";
+el.style.transform = "translateY(0px)";
+
+}
+
+});
+
+});
